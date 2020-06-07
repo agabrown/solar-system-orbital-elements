1a172733e7d554d5787c6678dcf03cd62736307c
agabrown <agabrown@xs4all.nl>
2020-06-07 10:34:54 +0200

    p5.gui.js for variables settings GUI

--- orbital-elements.js
+++ orbital-elements.js
@@ -1,23 +1,5 @@
-var plotWidth = 640;
-var plotHeight = 480;
-var controlsWidth = 300;
-var controlsHeight = plotHeight;
-var xOrigPlot = 0.1*plotWidth;
-var yOrigPlot = 0.9*plotHeight;
-var xAxisLen = 0.8*plotWidth;
-var yAxisLen = 0.8*plotHeight;
-
-var inclDefault = 0.0;
-var inclMin = -90;
-var inclMax = 90;
-
-var sliderWidth = 240;
-var sliderHeight = 30;
-var inclSliderX = plotWidth+10;
-var inclSliderY = 0.85*controlsHeight;
-
-var inputHSize = 75;
-var inputVSize = 18;
+var plotWidth = 800;
+var plotHeight = 600;
 
 var paddingHorizontal = 50;
 var paddingVertical = 50;
@@ -25,8 +7,24 @@ var paddingVertical = 50;
 var longRightwardsArrow = "";
 var longLeftwardsArrow = "";
 var inclLabel = "Inclination = ";
+var ascnodeLabel = "Longitude ascending node = ";
+var argperiLabel = "Argument perihelion = ";
 
-var inclination;
+var inclination = 0;
+var inclinationMin = -90;
+var inclinationMax = 90;
+var inclinationStep = 1;
+
+var ascendingNode;
+var ascendingNodeMin = 0;
+var ascendingNodeMax = 360;
+var ascendingNodeStep = 1;
+
+var argPerihelion;
+var argPerihelionMin = 0;
+var argPerihelionMax = 360;
+var argPerihelionStep = 1;
+
 var explanationText;
 
 var sketch = function(p) {
@@ -39,32 +37,16 @@ var sketch = function(p) {
         var heading = p.createElement('h2', 'Orbital elements for solar system objects');
         heading.position(paddingHorizontal+0.1*plotWidth, paddingVertical-55);
         heading.style("margin",0);
-        var canvas = p.createCanvas(plotWidth + controlsWidth, plotHeight);
+        var canvas = p.createCanvas(plotWidth, plotHeight);
         canvas.position(paddingHorizontal, paddingVertical);
         var explain = p.createDiv(p.join(explanationText, " "));
         explain.position(paddingHorizontal+20, paddingVertical+plotHeight+20);
-        explain.size(plotWidth + controlsWidth - 40);
+        explain.size(plotWidth - 40);
+
+        gui = createGui('slider-range-1');
+        gui.addGlobals('inclination', 'ascendingNode', 'argPerihelion');
 
         p.textSize(16);
-
-        inclSlider = p.createSlider(inclMin, inclMax, inclDefault, 0);
-        inclSlider.position(inclSliderX + paddingHorizontal, inclSliderY + paddingVertical);
-        inclSlider.style('width', sliderWidth.toFixed(0)+'px');
-        inclSlider.style('height', sliderHeight.toFixed(0)+'px');
-        inclSlider.style('color', '#00FFFF');
-        inclSlider.mouseMoved(inclSliderMoved);
-
-        inclInput = p.createInput(inclDefault.toFixed(0), "number");
-        inclInput.position(inclSliderX + paddingHorizontal + p.textWidth(inclLabel), inclSliderY + paddingVertical+sliderHeight);
-        inclInput.style("font-size", "16px");
-        inclInput.size(inputHSize, inputVSize);
-        inclInput.changed(inclInputChanged);
-        inclnation = inclInput.value();
-
-        resetButton = p.createButton("Reset");
-        resetButton.position(inclSliderX + paddingHorizontal + 10, inclSliderY + paddingVertical-2*sliderHeight);
-        resetButton.style("font-size", "16px");
-        resetButton.mousePressed(reset);
     }
 
     p.draw = function() {
@@ -88,24 +70,3 @@ var sketch = function(p) {
 
 var myp5 = new p5(sketch);
 
-function inclInputChanged() {
-    if (inclInput.value() < inclMin) {
-        inclInput.value(inclMin.toFixed(0));
-    }
-    if (inclInput.value() > inclMax) {
-        inclInput.value(inclMax.toFixed(0));
-    }
-    inclination = inclInput.value();
-    inclSlider.value(inclination);
-}
-
-function inclSliderMoved() {
-    inclination = inclSlider.value();
-    inclInput.value(inclination.toFixed(0));
-}
-
-function reset() {
-    inclination = inclDefault;
-    inclSlider.value(inclination);
-    inclInput.value(inclination.toFixed(0));
-}
